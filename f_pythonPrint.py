4aada350d28aee05cf126d3c2cf4b2d9d79164b9
Nguyen Tuan Anh <30680551+NguyenTuanAnh2195@users.noreply.github.com>
2018-02-04 10:15:25 +0700

    Create f_pythonPrint.py

---
+++ f_pythonPrint.py
@@ -0,0 +1,7 @@
+Print("Python code wooooo!")*10
+
+mes = str(input("Type something and I'll make it print !!!")) #message
+rep = int(input("How many times do you want it to print? ")) #repetition
+
+for i in range(rep):
+    print(mes)
